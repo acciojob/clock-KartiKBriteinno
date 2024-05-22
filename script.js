f001d68561d36d6773c66e9ce389728d0160cf41
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-05-22 17:19:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+  function updateTimer() {
+      const timerElement = document.getElementById('timer');
+      const now = new Date();
+      const formattedTime = now.toLocaleString();
+      timerElement.textContent = formattedTime;
+    }
+    
+    document.addEventListener('DOMContentLoaded', () => {
+      updateTimer();
+      setInterval(updateTimer, 1000);
+    });
